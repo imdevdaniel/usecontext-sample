03b1feac6a0327ea664960bca5579323cd7c3fda
Daniel Pacheco <imdevdaniel@gmail.com>
2022-12-16 23:01:06 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Home } from './components/containers/Home/Home';
+import { MenuContext } from './contexts/MenuContext';
+
 
 function App() {
+  const [menuItems, setMenuItems] = useState({ items: [] });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MenuContext.Provider value={{ menuItems, setMenuItems }}>
+      <Home />
+    </MenuContext.Provider>
+
   );
 }
 
